785b2d02a82300f663cb5bc1c1ba467877597cec
Maria Eduarda de Souza Ferreira <49918920823@>
2024-03-18 16:50:17 -0300

    css template

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 // responsavel por sobrepor o dom do html e da acesso para manipular qualquer elementos 
 import ReactDOM from 'react-dom/client'
 // importa o componete
-import App from './App.jsx'
-import './global.css'
+import { App } from './App'
+
 
 // o reactdom enxerga os elementos html da página e prossibilita a manipulação
 ReactDOM.createRoot(document.getElementById('root')).render(
---
+++ src/paginas/Inicial.jsx
@@ -0,0 +1,17 @@
+import { Cabecalho } from '../componentes/Cabecalho' 
+import { Lateral } from '../componentes/Lateral'
+import { Conteudo } from '../componentes/Conteudo'
+import estilos from './Inicial.module.css'
+
+export function Inicial() {
+
+  return (
+      <div className={estilos.gridConteiner}>
+        <Cabecalho/>
+        <Lateral/>
+        <Conteudo/>
+      </div>
+  )
+}
+
+
